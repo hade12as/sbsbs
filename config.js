9e6eb60069ed493229ac67ca0d483b0e77971457
hade12as <116930238+hade12as@users.noreply.github.com>
2024-04-17 00:24:45 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  bot: {
+    owners: ["948386383502901249"],  // ايدي الاونر
+    botID: "1035662460729958510",    // ايدي البوت
+    GuildId: "1081974415316746400",   // ايدي السيرفير
+    ClientId: "1035662460729958510",    // ايدي البوت
+    serverinvte: "https://discord.gg/YfJaGyPu5W", // انفايت سيرفر
+    clientSECRET: "_rB3ubIYu7d8urJcWVB90s1_3fyUKAYo" , // سكريت
+    callbackURL: "https://candy-thinkable-possum.glitch.me/login", // الكال باك
+    inviteBotUrl: "https://discord.com/oauth2/authorize?client_id=1035662460729958510&permissions=8&scope=bot",// انفايت البوت
+    TheLinkVerfy : 'https://discord.com/oauth2/authorize?client_id=1035662460729958510&response_type=code&redirect_uri=https%3A%2F%2Fcandy-thinkable-possum.glitch.me%2Flogin&scope=identify+email+guilds+guilds.join', // رابط اوثو رايز بالصلاحيه ادخال الي سيرفرات
+    prefix : '$', 
+    ceatogry : '1116853350009405512', // كاتوجري الي يفتح فيها تكت شراء
+     TOKEN: "MTAzNTcxNTk4ODYzODkyODk4OA.GiPhQk.6npC3VSlMYnE202P18S1pYyaDNRoin-ZUpKU6g",// توكن 
+    Price: 1,    // سعر العضو الواحد
+    TraId  : '948386383502901249' // ايدي الي يتحوله كريديت
+  },
+  website: {
+    PORT: "3001",
+  }
+}
